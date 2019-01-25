398dbb97b9a734399fa78297d240de2bc9e80fe2
arsenalahm <clevercake7@gmail.com>
2019-01-25 14:30:31 +0800

    Codes for data analysis

---
+++ code/data-analysis/main.py
@@ -0,0 +1,75 @@
+import os
+import sys
+from matplotlib import pyplot as plt
+import math
+import numpy as np
+import pandas as pd
+from point import Point
+from frames import Frames
+import scipy.signal as signal
+from sklearn.decomposition import PCA
+from scipy.stats import skew, kurtosis
+
+def get_file_list():
+	root_dir = '.'
+	file_name = os.listdir(root_dir)
+	
+	file_list = []
+	for i in range(0, len(file_name)):
+		path = os.path.join(root_dir, file_name[i])
+		if os.path.isfile(path) and path.split('.')[-1] == 'log':
+			file_list.append(path)
+
+	return file_list
+
+def draw_points(timestamp, points, touch):
+	n = len(timestamp)
+	assert n == len(points)
+	x, y, z = Point.points_2_xyz(points)
+	plt.plot(timestamp, x, timestamp, y, timestamp, z, timestamp, touch)
+
+def draw_peaks(timestamp, points):
+	x, y, z = Point.points_2_xyz(points)
+	peaks, _ = signal.find_peaks(abs(x), height=1000, distance=5, threshold=5)
+	plt.plot(timestamp[peaks], x[peaks], '.')
+	peaks, _ = signal.find_peaks(abs(y), height=1000, distance=5, threshold=5)
+	plt.plot(timestamp[peaks], y[peaks], '.')
+	peaks, _ = signal.find_peaks(abs(z), height=1000, distance=5, threshold=5)
+	plt.plot(timestamp[peaks], z[peaks], '.')
+
+def draw_frames(frames):
+	timestamp = frames.timestamp
+	acc = frames.acc
+	gyr = frames.gyr
+	touch = frames.touch
+
+	plt.figure(file)
+
+	plt.subplot(2, 1, 1)
+	plt.title('acc')
+	draw_points(timestamp, acc, np.array(touch) * 500)
+	print frames.touch
+	draw_peaks(timestamp, acc)
+
+	plt.subplot(2, 1, 2)
+	plt.title('gyr')
+	draw_points(timestamp, gyr, np.array(touch) * 100)
+	# key_gyr = frames.caln_key_frame()
+	# plt.axvline(key_gyr - 100, color = 'green')
+	# plt.axvline(key_gyr + 100, color = 'green')
+	draw_peaks(timestamp, gyr)
+
+	plt.show()
+
+if __name__ == '__main__':
+	# file_list = get_file_list()
+	# for file in file_list:
+	# 	frames = Frames()
+	# 	frames.read_data(file)
+	# 	frames.preprocess()
+	# 	draw_frames(frames)
+	file = 'lzp-index-index.log'
+	frames = Frames()
+	frames.read_data(file)
+	frames.preprocess()
+	draw_frames(frames)
---
+++ code/data-analysis/point.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+class Point:
+	x = 0.0
+	y = 0.0
+	z = 0.0
+
+	def __init__(self, *args):
+		if (len(args) == 3):
+			self.x = float(args[0])
+			self.y = float(args[1])
+			self.z = float(args[2])
+		else:
+			self.x = 0.0
+			self.y = 0.0
+			self.z = 0.0
+	
+	def __str__(self):
+		return '(' + str(self.x) + ', ' + str(self.y) + ', ' + str(self.z) + ')'
+
+	@staticmethod
+	def points_2_xyz(array):
+		n = len(array)
+		x = np.array([array[i].x for i in range(n)])
+		y = np.array([array[i].y for i in range(n)])
+		z = np.array([array[i].z for i in range(n)])
+		return x, y, z
+
+	@staticmethod
+	def xyz_2_points(x, y, z):
+		n = len(x)
+		assert n == len(y)
+		assert n == len(z)
+		points = [Point(x[i], y[i], z[i]) for i in range(n)]
+		return points
